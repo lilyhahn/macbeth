9b919786b77eee0d2952ce96f155ed2db5f11dc0
Toby Hahn <thbike17@gmail.com>
2013-04-10 20:52:30 -0400

    Now knows when there is no internet connection, and tries to run existing script. Switched launcher to completely AppleScript.

--- fix_icon.sh
+++ fix_icon.sh
@@ -2,4 +2,5 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-cp icon.icns Macbeth.app/Contents/Resources/AutomatorApplet.icns
+cp icon.icns Macbeth.app/Contents/Resources/applet.icns
+
